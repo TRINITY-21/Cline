b9d54c14aeee1bf219fd216252c1c9f637e8bcf4
TRINITY-21 <agyemanjoseph12@gmail.com>
2025-11-01 05:10:19 +0300

    new date changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8ZS3H6BW5N" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8ZS3H6BW5N');
+            `,
+          }}
+        />
+        {/* Google AdSense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7225325372988376"
